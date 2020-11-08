aa7e0c5c5f92cef5795265ec475508598d1112c3
Johanna <jforsta@gmail.com>
2020-11-08 11:06:52 +0200

    checkstyle testaus

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -72,6 +72,6 @@ public class Varasto {
     
     // --- Merkkijonoesitys Varasto-oliolle: ----
     public String toString() {
-        return ("saldo = " + saldo + ", vielä tilaa " + paljonkoMahtuu());
+        return ("saldo = " + saldo + ", vielä tilaa on" + paljonkoMahtuu());
     }
 }
